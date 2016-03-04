b8354c47ab4c6b939ca8a0427e97e2074ad7a12c
Garrett <>
2016-03-03 20:43:15 -0800

    Super Classes Desert Menu View

--- src/citbyui/cit260/desertescape/view/DesertMenuView.java
+++ src/citbyui/cit260/desertescape/view/DesertMenuView.java
@@ -11,63 +11,45 @@ import java.util.Scanner;
  *
  * @author Garrett
  */
-public class DesertMenuView {
+public class DesertMenuView extends View {
     
-    public void desertMenu(){
-    boolean run = false;
-        while (run != true)
-        {
-            displayOptions();
-            run = getOption();
-            
-        }          
-    }
-    
-    public void displayOptions(){
-        
-        String options = "";
-        options = "========================"
+    public DesertMenuView(){
+    super("========================"
                 + "\nRun"
                 + "\nInspect"
                 + "\nFill Pit"
                 + "\nMain Menu"
-                + "\n========================";
-        System.out.println(options);
+                + "\n========================");
+    
     }
     
-    public boolean getOption(){
-        
-        boolean run = false; 
-        boolean isValidOption = false;
-        Scanner keyboard = new Scanner(System.in);
-        
-        System.out.println("What would you like to do?");
-        
-        while (!isValidOption)
-        {
-            String input = keyboard.nextLine();
-            if (input.isEmpty()){
-            }
-            else {
-                input = input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
-                if ("R".equals(input) || "Run".equals(input))
-                {
-                    run = true;
-                    RunMenuView runMenu = new RunMenuView();
-                        runMenu.runMenu();
-                }
-                else if ("I".equals(input) || "Inspect".equals(input))
-                    System.out.println("NEED TO IMPLEMENT ITEMS AND COLLECTION CONTROLLER");
-                else if ("F".equals(input) || "Fill pit".equals(input))
-                    System.out.println("CALL THE FILL PIT VIEW");
-                else if ("M".equals(input) || "Main menu".equals(input))
-                {
-                    MainMenuView mainMenu = new MainMenuView();
+    
+    
+    @Override
+    public boolean doAction(String value) {
+        boolean run = false;
+        switch (value) {
+            case "Run":
+                run = true;
+                RunMenuView runMenu = new RunMenuView();
+                runMenu.runMenu();
+                break;
+            case "Inspect":
+                System.out.println("NEED TO IMPLEMENT ITEMS AND COLLECTION CONTROLLER");
+                break;
+            case "Fill Pit":
+                System.out.println("CALL THE FILL PIT VIEW");
+                break;
+            case "Main Menu":
+                MainMenuView mainMenu = new MainMenuView();
                     mainMenu.display();
-                }
-            }
-            
-        }    
+                break;
+            default:
+                run = false;
+                System.out.println("Invalid option");
+                return run;
+        }
         return run;
     }
+
 }
