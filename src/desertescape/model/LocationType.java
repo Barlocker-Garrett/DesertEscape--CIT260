26c736f36d20b7fa3cd78a356e2cc067c3b81fe8
Garrett <>
2016-03-11 16:32:44 -0800

    Bug in displaying Map

---
+++ src/desertescape/model/LocationType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desertescape.model;
+
+/**
+ *
+ * @author Garrett
+ */
+public enum LocationType {
+    Desert,
+    Pyramid,
+    Cave,
+    Alien_Camp,
+    Cliffs,
+    Shop,
+    Mountain
+} 
