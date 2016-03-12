33b9b4b080a1700e30db7ab8b11a5fc114bcd07d
Garrett <>
2016-03-11 16:32:35 -0800

    Bug in displaying Map

--- src/desertescape/DesertEscape.java
+++ src/desertescape/DesertEscape.java
@@ -5,8 +5,6 @@
  */
 package desertescape;
 
-import citbyui.cit260.desertescape.view.DesertMenuView;
-import citbyui.cit260.desertescape.view.RunMenuView;
 import citbyui.cit260.desertescape.view.StartProgramView;
 import desertescape.model.Game;
 import desertescape.model.Item;
@@ -28,34 +26,6 @@ public class DesertEscape {
         
         StartProgramView startProgram = new StartProgramView();
         startProgram.startProgram();
-        
-        //Creation of an instance for Player
-        Player playerOne = new Player();
-        
-        playerOne.setName("Joe");
-        playerOne.setNumLives(3);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        //Creation of an instance for Game
-        Game gameOne = new Game();
-        
-        gameOne.setTotalTime(0);
-        gameOne.setDifficulty(1);
-        
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-        //Creation of an instance for Item
-        Item food = new Item();
-        
-        food.setName("Agave");
-        food.setDescription("Its flowers and flower buds are edible");
-        food.setValue(20);
-        
-        String itemInfo = food.toString();
-        System.out.println(itemInfo);
     }
 
     public static Player getPlayer() {
