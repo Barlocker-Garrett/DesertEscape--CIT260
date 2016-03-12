5b5f777160b10522cb25181a9afa8a7ae7556866
Garrett <>
2016-03-12 13:07:30 -0800

    Added movement controller and needed views Removed Run Menu

--- src/citbyui/cit260/desertescape/view/GameMenuView.java
+++ src/citbyui/cit260/desertescape/view/GameMenuView.java
@@ -5,37 +5,38 @@
  */
 package citbyui.cit260.desertescape.view;
 
+import citbyui.cit260.desertescape.control.MovementController;
 import desertescape.DesertEscape;
+import desertescape.model.Location;
 
 /**
  *
  * @author Garrett
  */
 public class GameMenuView extends View {
-    
-    public GameMenuView(){
-    super("========================"
-                + "\nRun"
-                + "\nInspect"
-                + "\nFill Pit"
-                + "\nMain Menu"
-                + "\nView Map"
+
+    public GameMenuView() {
+        super("========================"
+                + "\nN - Run North"
+                + "\nE - Run East"
+                + "\nS - Run South"
+                + "\nW - Run West"
+                + "\nI - Inspect"
+                + "\nF - Fill Pit"
+                + "\nM - Main Menu"
+                + "\nD - Display Game Menu"
+                + "\nV - View Map"
+                + "\nC - Current Location"
                 + "\n========================");
-   
+
     }
-    
-    
-    
+
     @Override
     public boolean doAction(String value) {
-        
+
         char charSel = value.toUpperCase().charAt(0);
-        
+
         switch (charSel) {
-            case 'R':
-                RunMenuView runMenu = new RunMenuView();
-                runMenu.display();
-                break;
             case 'I':
                 System.out.println("NEED TO IMPLEMENT ITEMS AND COLLECTION CONTROLLER");
                 break;
@@ -44,10 +45,29 @@ public class GameMenuView extends View {
                 break;
             case 'M':
                 MainMenuView mainMenu = new MainMenuView();
-                    mainMenu.display();
+                mainMenu.display();
+                break;
+            case 'D':
+                GameMenuView desertMenu = new GameMenuView();
+                desertMenu.display();
                 break;
             case 'V':
                 viewMap();
+                break;
+            case 'N':
+                moveNorth();
+                break;
+            case 'E':
+                moveEast();
+                break;
+            case 'S':
+                moveSouth();
+                break;
+            case 'W':
+                moveWest();
+                break;
+            case 'C':
+                currentLocation();
                 break;
             case 'Q':
                 return true;
@@ -57,9 +77,38 @@ public class GameMenuView extends View {
         return false;
     }
 
-    private void viewMap() {
+    public void viewMap() {
         String map = DesertEscape.getGame().getMap().getMapString();
         System.out.println(map);
+    }
+
+    private void moveNorth() {
+        MovementController mc = new MovementController();
+        mc.moveNorth(DesertEscape.getGame());
+        currentLocation();
+    }
+
+    private void moveEast() {
+        MovementController mc = new MovementController();
+        mc.moveEast(DesertEscape.getGame());
+        currentLocation();
+    }
+
+    private void moveSouth() {
+        MovementController mc = new MovementController();
+        mc.moveSouth(DesertEscape.getGame());
+        currentLocation();
+    }
+
+    private void moveWest() {
+        MovementController mc = new MovementController();
+        mc.moveWest(DesertEscape.getGame());
+        currentLocation();
+    }
+
+    private void currentLocation() {
+        Location l = DesertEscape.getGame().getPlayer().getLocation();
+        System.out.println("You are at: (" + l.getRow() + ", " + l.getCol() + ")");
     }
 
 }
