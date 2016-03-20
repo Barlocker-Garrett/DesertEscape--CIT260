8166a7949d85095c02f62b9d45b4c1c3f22084c9
Garrett <Garrett@Tech-Garrett>
2016-03-19 17:23:20 -0700

    FillPitException

--- src/citbyui/cit260/desertescape/control/FillPitControl.java
+++ src/citbyui/cit260/desertescape/control/FillPitControl.java
@@ -5,34 +5,39 @@
  */
 package citbyui.cit260.desertescape.control;
 
+import desertescape.exceptions.FillPitException;
+
 /**
  *
  * @author Garrett
  */
 public class FillPitControl {
 
-    public static long FillPit(long shovelFull, long height, long width, long length) {
+    public static void FillPit(long shovelFull, long height, long width, long length) {
         double shovelSize = 0.8;
         long gallons;
         gallons = (long) Math.round(height * width * length * 0.133680555556);
 
         long neededSand = (long) (Math.round(gallons / shovelSize));
-
+        try {
+            if (shovelFull < 0) {
+                throw new FillPitException("Must be possitive number");
+            }
+            if (shovelFull == 0) {
+                throw new FillPitException("Must be more than 0");
+            }
+            if (neededSand > shovelFull) {
+                throw new FillPitException("You needed more sand");
+            }
+            if (neededSand < shovelFull) {
+                throw new FillPitException("You overfilled the pit");
+            }
+        } catch (FillPitException fp) {
+            System.out.println(fp.getMessage());
+        }
+        
         if (neededSand == shovelFull) {
-            return neededSand;
-        }
-        if (shovelFull < 0) {
-            return 0;
-        }
-        if (shovelFull == 0) {
-            return 0;
-        }
-        if (neededSand > shovelFull) {
-            return 0;
-        }
-        if (neededSand < shovelFull) {
-            return 0;
-        }
-        return -1;
+                System.out.println("Correct, you filled the pit");
+            }
     }
 }
