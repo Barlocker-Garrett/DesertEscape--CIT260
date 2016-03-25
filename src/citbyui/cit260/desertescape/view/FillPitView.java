5627328fb6c29c0c9d8ff51ad4b689c4c8146d3f
Garrett <Garrett@Tech-Garrett>
2016-03-24 22:13:12 -0700

    Fixed Fill Pit View

--- src/citbyui/cit260/desertescape/view/FillPitView.java
+++ src/citbyui/cit260/desertescape/view/FillPitView.java
@@ -6,18 +6,19 @@
 package citbyui.cit260.desertescape.view;
 
 import citbyui.cit260.desertescape.control.FillPitControl;
+import desertescape.exceptions.FillPitException;
 import java.util.Scanner;
 
 /**
  *
  * @author Garrett
  */
-public class FillPitView extends View{
-    
+public class FillPitView extends View {
+
     public FillPitView() {
         super("You need to cross the pit");
     }
-    
+
     @Override
     public boolean doAction(String value) {
         long height;
@@ -27,34 +28,38 @@ public class FillPitView extends View{
         long length;
         length = ((long) (1 + Math.random() * 100));
         console.println("Which is " + height + " feet deep"
-                           + "\n" + width + " feet wide and"
-                           + "\n" + length + " feet long");
-        console.println("With a shovel that can scoop 0.8 gallons" +
-                         "\nHow many scoops will it take to fill the pit?" +
-                         "\nHint: Constant to convert cubic feet to gallons is:" +
-                         "~0.13368");
+                + "\n" + width + " feet wide and"
+                + "\n" + length + " feet long");
+        console.println("With a shovel that can scoop 0.8 gallons"
+                + "\nHow many scoops will it take to fill the pit?"
+                + "\nHint: Constant to convert cubic feet to gallons is:"
+                + "~0.13368");
         long shovelFull = (long) getDouble();
-        FillPitControl.FillPit(shovelFull, height, width, length);
+        try {
+            FillPitControl.FillPit(shovelFull, height, width, length);
+        } catch (FillPitException fp) {
+            console.println(fp.getMessage());
+        }
         return true;
     }
-    
+
     @Override
-        public String getInput() {
+    public String getInput() {
         return "Fill Pit";
     }
-    
+
     @Override
-        public double getDouble() {
+    public double getDouble() {
         Scanner keyboard = new Scanner(System.in);
         double input = 0.0;
 
-            try {
-                input = keyboard.nextDouble();
-            } catch (NumberFormatException nf) {
-                console.println("Invalid input - please enter a valid number" +
-                                    "Try again or enter Q to quit.");
+        try {
+            input = keyboard.nextDouble();
+        } catch (NumberFormatException nf) {
+            console.println("Invalid input - please enter a valid number"
+                    + "Try again or enter Q to quit.");
         }
         return input;
     }
-    
+
 }
